335d7a6b47de9b0cae817f93f9d5ac4f2a757305
Lucas Figueiredo <lucasborges_06@hotmail.com>
2022-07-06 11:01:20 -0300

    Criando projeto, tab de mudanca de pagina e 2 paginas

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppTab from './AppTab';
+
+export default function App() {
+  return (
+    <>
+      <AppTab />
+      <StatusBar style="light" />
+    </>
+    
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  }
+});
+
+
